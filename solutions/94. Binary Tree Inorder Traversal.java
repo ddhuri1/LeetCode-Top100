1160045001241ee6ca0d89029407937f22b6dd22
Devina Dhuri <ddhuri1@binghamton.edu>
2020-06-20 19:23:07 -0400

    0 ms

---
+++ solutions/94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public List < Integer > inorderTraversal(TreeNode root)
+    {
+        List <Integer> res = new ArrayList < > ();
+        Stack <TreeNode> stack = new Stack < > ();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) 
+        {
+            while (current != null)
+            {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            res.add(current.val);
+            current = current.right;
+        }
+        return res;
+    }
+}
