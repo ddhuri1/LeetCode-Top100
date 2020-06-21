3d4ce63d9d16195fffa2e6de156d932c9c1ac0b8
Devina Dhuri <ddhuri1@binghamton.edu>
2020-06-21 19:00:41 -0400

    10 ms

---
+++ solutions/322. Coin Change.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int coinChange(int[] coins, int amount) 
+    {
+        int[] dp = new int[amount + 1]; 
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0; 
+        int min = Integer.MAX_VALUE;
+        for(int a = 1; a <= amount; a++) 
+        {
+            for(int coin : coins) 
+            {
+                if(coin <= a)
+                    dp[a] = Math.min(dp[a], 1+ dp[a - coin]);
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
